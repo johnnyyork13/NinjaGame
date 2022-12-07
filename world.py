0f5da92e324a4ee54be22916b1a5d0cb44b34341
johnnyyork13 <johnnyyork13@hotmail.com>
2022-12-06 20:45:49 -0500

    make everything fancy

--- world.py
+++ world.py
@@ -9,6 +9,11 @@ class World:
         self.win = win
         self.map = [[], [], []]
         self.current_level = level_one
+        self.ladder = pg.image.load("ladder.png")
+        self.border = pg.image.load("border.png")
+        self.basic_tile = pg.image.load("basic_tile.png")
+        self.door = pg.image.load("door.png")
+        self.door_first_half = False
 
     def check_map(self, state):
         #change displayed map
@@ -33,17 +38,23 @@ class World:
                 
 
     def draw_map(self):
-        for tiles in self.map[0]:
+        self.door_first_half = False
+        for tile_id, tiles in enumerate(self.map[0]):
             if tiles[1] == 1:
-                pg.draw.rect(self.win, BLUE, tiles[0])
+                self.win.blit(self.basic_tile, (tiles[0][0], tiles[0][1]))
+                #pg.draw.rect(self.win, BLUE, tiles[0])
             elif tiles[1] == 2:
-                pg.draw.rect(self.win, DARKBLUE, tiles[0])
-            elif tiles[1] == 3:
-                pg.draw.rect(self.win, GRAY, tiles[0])
+                self.win.blit(self.border, (tiles[0][0], tiles[0][1]))
+                #pg.draw.rect(self.win, DARKBLUE, tiles[0])
+            elif tiles[1] == 3 and not self.door_first_half:
+                self.win.blit(self.door, (tiles[0][0], tiles[0][1]))
+                self.door_first_half = True
+                #pg.draw.rect(self.win, GRAY, tiles[0])
             elif tiles[1] == 4:
                 pg.draw.rect(self.win, YELLOW, (tiles[0][0] + COIN_OFFSET, tiles[0][1] + COIN_OFFSET, COIN_SIZE, COIN_SIZE) )
             elif tiles[1] == 5:
-                pg.draw.rect(self.win, BROWN, tiles[0], 2)
+                self.win.blit(self.ladder, (tiles[0][0], tiles[0][1]))
+                #pg.draw.rect(self.win, BROWN, tiles[0], 2)
             elif tiles[1] == 6:
                 pg.draw.rect(self.win, RED, tiles[0])
 
